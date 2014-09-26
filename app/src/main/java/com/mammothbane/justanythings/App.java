7010d5e3f3cc4955b82d6b594d7f48e9d16326b5
mammothbane <itsamammoth@gmail.com>
2014-09-26 19:34:10 -0400

    initial commit

---
+++ app/src/main/java/com/mammothbane/justanythings/App.java
@@ -0,0 +1,9 @@
+package com.mammothbane.justanythings;
+
+import android.app.Application;
+
+/**
+ * Created by mammothbane on 9/26/2014.
+ */
+public class App extends Application {
+}
---
+++ app/src/main/java/com/mammothbane/justanythings/activities/MainActivity.java
@@ -0,0 +1,11 @@
+package com.mammothbane.justanythings.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by mammothbane on 9/26/2014.
+ */
+public class MainActivity extends Activity {
+
+
+}
