68f0c389e63dffafb555d5aa54723a55c74d5f42
mammothbane <itsamammoth@gmail.com>
2014-09-26 21:34:07 -0400

    didn't compile with custom namespace

--- app/src/main/java/com/mammothbane/justanythings/activities/MainActivity.java
+++ app/src/main/java/com/mammothbane/justanythings/activities/MainActivity.java
@@ -3,16 +3,15 @@ package com.mammothbane.justanythings.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
 
+import com.commonsware.cwac.camera.CameraFragment;
+import com.commonsware.cwac.camera.CameraHost;
+import com.commonsware.cwac.camera.SimpleCameraHost;
 import com.mammothbane.justanythings.R;
-import com.mammothbane.justanythings.views.CameraPreview;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 import butterknife.OnClick;
 
 /**
@@ -20,43 +19,19 @@ import butterknife.OnClick;
  */
 public class MainActivity extends Activity {
 
-    private Camera mCamera;
-    private boolean choosingPhoto = false;
-    private CameraPreview cameraPreview;
-    @InjectView(R.id.fl_camera) FrameLayout frameLayout;
+    CameraFragment cameraFragment;
+    CameraHost cameraHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+        cameraFragment = (CameraFragment)getFragmentManager().findFragmentByTag("frag_camera");
+        cameraHost = new SimpleCameraHost.Builder(this).useFrontFacingCamera(false).photoDirectory(getCacheDir()).useFullBleedPreview(true).build();
+        cameraFragment.setHost(cameraHost);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (hasCamera()) {
-            mCamera = getCameraInstance();
-            cameraPreview = new CameraPreview(this, mCamera);
-            frameLayout.addView(cameraPreview);
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (cameraPreview != null && frameLayout.getChildAt(0) == cameraPreview) frameLayout.removeView(cameraPreview);
-        if (mCamera != null) {
-            mCamera.release();
-            mCamera = null;
-        }
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -67,17 +42,9 @@ public class MainActivity extends Activity {
         return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
 
-    private static Camera getCameraInstance() {
-        Camera camera = null;
-        try {
-            camera = Camera.open();
-        } catch (Exception ignore) {}
-        return camera;
-    }
 
     @OnClick(R.id.ib_choose)
     public void choosePhoto(View view) {
-        choosingPhoto = true;
         Intent intent = new Intent();
 
     }
