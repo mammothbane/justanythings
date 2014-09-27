e56ae8e85ce3d4c4e142a771de8fe531a401e498
mammothbane <itsamammoth@gmail.com>
2014-09-27 02:08:09 -0400

    end of day commit

--- app/src/main/java/com/mammothbane/justanythings/App.java
+++ app/src/main/java/com/mammothbane/justanythings/App.java
@@ -6,4 +6,10 @@ import android.app.Application;
  * Created by mammothbane on 9/26/2014.
  */
 public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
 }
--- app/src/main/java/com/mammothbane/justanythings/fragments/CameraFragment.java
+++ app/src/main/java/com/mammothbane/justanythings/fragments/CameraFragment.java
@@ -1,34 +1,64 @@
 package com.mammothbane.justanythings.fragments;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-import com.commonsware.cwac.camera.CameraHost;
-import com.commonsware.cwac.camera.SimpleCameraHost;
+import com.commonsware.cwac.camera.CameraView;
 import com.mammothbane.justanythings.R;
+import com.mammothbane.justanythings.views.CameraPreview;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by mammothbane on 9/26/2014.
  */
 public class CameraFragment extends Fragment {
 
-    com.commonsware.cwac.camera.acl.CameraFragment cameraFragment;
-    CameraHost cameraHost;
+    @InjectView(R.id.fl_camera) FrameLayout frameLayout;
 
+    Camera camera;
+
+    CameraView cameraView;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        camera = getCamera();
+        cameraView = new CameraView(getActivity());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
-        cameraFragment = new com.commonsware.cwac.camera.acl.CameraFragment();
+        ButterKnife.inject(this, view);
 
-        //cameraFragment = ((com.commonsware.cwac.camera.acl.CameraFragment) getChildFragmentManager().findFragmentByTag("frag_camera"));
-        cameraHost = new SimpleCameraHost.Builder(getActivity()).useFrontFacingCamera(false).photoDirectory(getActivity().getCacheDir()).useFullBleedPreview(true).build();
-        cameraFragment.setHost(cameraHost);
-        getChildFragmentManager().beginTransaction().add(R.id.fl_camera, cameraFragment);
+        frameLayout.addView(new CameraPreview(getActivity(), camera));
+
         return view;
+    }
+
+    @Nullable
+    static Camera getCamera() {
+        Camera c = null;
+        try {
+            Camera.open();
+        } catch (Exception ignore) {}
+        return c;
     }
 }
